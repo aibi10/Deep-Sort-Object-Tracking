005e2f171d2390d00fd6c7dbe44a3e267c1404cc
ABHISHEK SINGH <45706760+aibi10@users.noreply.github.com>
2023-01-22 15:00:27 +0530

    Add files via upload

---
+++ utils/draw.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+from PIL import Image
+
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+
+
+def compute_color_for_labels(label):
+    """
+    Simple function that adds fixed color depending on the class
+    """
+    color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+    return tuple(color)
+
+
+def draw_boxes(img, bbox, identities=None, offset=(0,0)):
+    for i,box in enumerate(bbox):
+        x1,y1,x2,y2 = [int(i) for i in box]
+        x1 += offset[0]
+        x2 += offset[0]
+        y1 += offset[1]
+        y2 += offset[1]
+        # box text and bar
+        id = int(identities[i]) if identities is not None else 0    
+        color = compute_color_for_labels(id)
+        label = '{}{:d}'.format("", id)
+        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
+
+        # to add bounding box
+        cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
+        # to design id text
+        cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
+        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
+        # # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255))
+        # # sub_face = img[y:y + h, x:x + w]
+        #
+        # sub_face = img[y1,y1, x1:x1+t_size[0]+3]
+        # cv2.imshow("cropped", imcrop(img, box))
+    return img
+
+
+def pad_img_to_fit_bbox(img, x1, x2, y1, y2):
+    img = cv2.copyMakeBorder(img, - min(0, y1), max(y2 - img.shape[0], 0),
+                             -min(0, x1), max(x2 - img.shape[1], 0), cv2.BORDER_REPLICATE)
+
+    y2 += -min(0, y1)
+    y1 += -min(0, y1)
+    x2 += -min(0, x1)
+    x1 += -min(0, x1)
+    return img, x1, x2, y1, y2
+
+
+def imcrop(img, bbox):
+    x1, y1, x2, y2 = bbox
+    if x1 < 0 or y1 < 0 or x2 > img.shape[1] or y2 > img.shape[0]:
+        img, x1, x2, y1, y2 = pad_img_to_fit_bbox(img, x1, x2, y1, y2)
+    return img[y1:y2, x1:x2, :]
+
+
+
+# if __name__ == '__main__':
+#     for i in range(82):
+#         print(compute_color_for_labels(i))
